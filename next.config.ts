6dcf1a54193c44a64c64aa6f1aa47eda62970356
antroe42 <28271281+Anthony-Roe@users.noreply.github.com>
2025-03-27 20:29:02 -0400

    next version

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: ["http://admin.localhost:3000", "http://*.localhost:3000"],
-  experimental: {
-    nodeMiddleware: true, // Enables Node.js runtime for middleware
-  },
 };
 
 export default nextConfig;
